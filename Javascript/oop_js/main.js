8426bc9f0160e30445492dea087c3d3c1ce2c14a
Erickmurigi <ericmurigi04@gmail.com>
2024-05-18 18:28:39 +0300

    project management dashboard

---
+++ Javascript/oop_js/main.js
@@ -0,0 +1,19 @@
+// const getUser = ()=> {
+//     setTimeout(() => {
+//         return{
+//             name: 'Duncan'
+//         }
+    
+//     }, 2000);
+
+// }
+// const user = getUser
+
+function greet (){
+    alert('danger')
+}
+setTimeout(() => {
+    showAlert ()
+
+    }, 2000)
+console.log(greet)
